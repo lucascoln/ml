460a3d7eeae8a77cb4d395daec830eb7ed0c74e9
lucascoln <38281976+lucascoln@users.noreply.github.com>
2018-12-18 23:26:35 +0800

    Add files via upload

--- cnn.py
+++ cnn.py
@@ -56,12 +56,6 @@ model.add(Activation('sigmoid'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.3))
 
-model.add(Conv2D(64,(3,3), padding='same'))
-model.add(Activation('sigmoid'))
-model.add(Conv2D(64,(3,3)))
-model.add(Activation('sigmoid'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.3))
 
 model.add(Flatten())
 model.add(Dense(512))
