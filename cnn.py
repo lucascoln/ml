d3c4c1f9f0e9706d4b62839aa8d90598e1fb9716
lucascoln <38281976+lucascoln@users.noreply.github.com>
2018-12-18 22:14:45 +0800

    Add files via upload

---
+++ cnn.py
@@ -0,0 +1,74 @@
+from keras.datasets import cifar10
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import SGD, Adam, RMSprop
+import keras
+import matplotlib.pyplot as pit
+
+IMG_CHANNELS = 3
+IMG_ROWS = 32
+IMG_COLS = 32
+
+BATCH_SIZE = 128
+NB_EPOCH = 40
+NB_CLASSES = 10
+VERBOSE = 1
+VALIDATION_SPLIT = 0.2
+OPTIM = RMSprop()
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+Y_train = np_utils.to_categorical(y_train, NB_CLASSES)
+Y_test = np_utils.to_categorical(y_test, 10)
+ 
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+model = Sequential()
+model.add(Conv2D(32,(3,3), padding='same', input_shape=(IMG_ROWS, IMG_COLS, IMG_CHANNELS)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.3))
+
+model.add(Conv2D(64,(3,3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.3))
+
+model.add(Conv2D(64,(3,3), padding='same'))
+model.add(Activation('sigmoid'))
+model.add(Conv2D(64,(3,3)))
+model.add(Activation('sigmoid'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.3))
+
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy', optimizer=OPTIM, metrics=['accuracy'])
+model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=NB_EPOCH, validation_split=VALIDATION_SPLIT, verbose=VERBOSE)
+score = model.evaluate(X_test, Y_test, batch_size=BATCH_SIZE, verbose=VERBOSE)
+print('Test score:',score[0])
+print('Test accuracy:',score[1])
+
+model_json = model.to_json()
+open('cifar10_architecture2.json', 'w').write(model_json)
+model.save_weights('cifar10_weights2.h5',overwrite=True)
+
+
+
